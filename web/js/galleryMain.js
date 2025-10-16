cb74b3d78e47bf68ad5900527a517a25639f15cb
ches2010 <41281952+ches2010@users.noreply.github.com>
2025-10-16 14:57:31 +0800

    Rename galleryMain.js file path in comment

---
+++ web/js/galleryMain.js
@@ -0,0 +1,19 @@
+// web/js/galleryMain.js - 主入口
+function initGallery(settings) {
+    const core = new GalleryCore(settings);
+    const elements = new GalleryElements();
+    const renderer = new GalleryRenderer(core, elements);
+    const loader = new GalleryLoader(core, elements, renderer);
+    const modal = new GalleryModal(core, elements);
+    const events = new GalleryEvents(core, elements, loader, modal);
+
+    elements.validateRequiredElements();
+    
+    // 暴露一些方法供其他模块使用
+    window.loadImages = () => loader.loadImages();
+    window.refreshGallery = () => loader.loadImages();
+    window.openImageModal = (image) => modal.openImageModal(image);
+
+    // 初始加载图片
+    loader.loadImages();
+}
